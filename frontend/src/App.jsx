5d30d44189055654722acc9e782aa6f2a740a5f7
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-27 17:35:58 +0530

    setup frontend using vite and tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div className="">
+       <h3 className="text-2xl text-center font-sans">KanBorad</h3>
+      <div className="flex items-center justify-center pt-[200px] ">
+        <h1 className="text-4xl font-semibold">Welcome to Kanboard</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
