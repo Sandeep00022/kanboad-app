675776f4bf9cdc5df5e77d68f2b97aa862441887
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-28 11:16:47 +0530

    create profile page ui

--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { Avatar, Button } from "flowbite-react";
+import React from "react";
+import { useSelector } from "react-redux";
 
 const DashProfile = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div className="max-w-lg mx-auto p-3 w-full">
-      
+    <div className="  bg-[#F5F5F6]  flex flex-col items-center    p-3 w-full">
+      <div className=" flex items-center gap-2 mt-10">
+        <Avatar img={currentUser?.profilePicture} rounded />
+        <div>
+          <h4 className="text-xl ">{currentUser.username}</h4>
+          <h6 className="text-sm">{currentUser.email}</h6>
+        </div>
+      </div>
+      <button className="border-red-600 mt-3 bg-white p-1 text-red-600 rounded-lg w-[300px]">
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default DashProfile
+export default DashProfile;
