e4e1aa371a61efe1d380b97775369a6bd672f52f
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-03-02 17:02:49 +0530

    update viteConfig file with new url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://kanboad-app-4.onrender.com",
         changeOrigin: true,
         secure: false,
       },
