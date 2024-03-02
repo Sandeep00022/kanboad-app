b2e18efde5baf1e479027a92f13bafa5f64660c4
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-03-02 10:56:43 +0530

    fix: all board logic to render only invited and owner can visit and see boards

--- frontend/src/components/DashBoards.jsx
+++ frontend/src/components/DashBoards.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
 import DashCard from "./DashCard";
-import { Spinner } from "flowbite-react";
+import { Button, Spinner } from "flowbite-react";
 import { useDispatch, useSelector } from "react-redux";
 import { taskInSuccess } from "../redux/task/taskSlice";
+import { useNavigate } from "react-router-dom";
 
 const DashBoards = () => {
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const { boards } = useSelector((state) => state.task);
-  const {currentUser} = useSelector((state)=>state.user);
+  const { currentUser } = useSelector((state) => state.user);
 
-  
+  const navigate = useNavigate();
+
+  const accessCardUser = boards.filter(
+    (board) =>
+      board.createdBy === currentUser._id ||
+      board.users.includes(currentUser._id)
+  );
 
   const getBoards = async () => {
     try {
@@ -43,9 +50,23 @@ const DashBoards = () => {
     <div className=" bg-[#F5F5F6] p-3 w-full">
       <div className="mb-3">
         <h5 className="text-sm font-semibold">
-          All Boards ({   boards.length})
+          All Boards ({accessCardUser.length})
         </h5>
       </div>
+      {accessCardUser.length === 0 && (
+        <div className="flex flex-col justify-center items-center mt-5">
+          <p className="text-xl font-semibold">
+            You don't have any Board, Creat one by visiting Home!
+          </p>
+          <Button
+            className="mt-4"
+            color="blue"
+            onClick={() => navigate("/dashboard?tab=dash")}
+          >
+            Home
+          </Button>
+        </div>
+      )}
       <div className="flex gap-2 flex-wrap">
         {boards &&
           boards.map((board) => <DashCard key={board._id} board={board} />)}
