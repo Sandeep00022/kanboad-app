194eae15efd8c77f9ab14654a35ccb8a83049730
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-03-02 17:37:45 +0530

    relocate backend package.json and nodemodules

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://kanboad-app-4.onrender.com",
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
