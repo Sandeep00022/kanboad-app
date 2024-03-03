26a6a7363449c7095bd3c83df24457acd831db60
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-03-03 08:27:10 +0530

    update: firebase auth config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -8,12 +8,12 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "kanban-app-fe8f1.firebaseapp.com",
-  projectId: "kanban-app-fe8f1",
-  storageBucket: "kanban-app-fe8f1.appspot.com",
-  messagingSenderId: "499260516676",
-  appId: "1:499260516676:web:4fb72347f5ad9699e14f11",
-  measurementId: "G-H8TQEF0MYS",
+  authDomain: "kanban-project-6c073.firebaseapp.com",
+  projectId: "kanban-project-6c073",
+  storageBucket: "kanban-project-6c073.appspot.com",
+  messagingSenderId: "920344154320",
+  appId: "1:920344154320:web:3c66f832a5de7506d68ed5",
+  measurementId: "G-S69VYPM7LZ"
 };
 
 // Initialize Firebase
